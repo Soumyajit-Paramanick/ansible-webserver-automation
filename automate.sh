4ec336c1d67a4cf5209700e4e6747586cf164f40
Soumyajit Paramanick <soumyajitparamanick96419@gmail.com>
2026-04-05 19:10:56 +0530

    Pushing the yml files and all the other things

---
+++ automate.sh
@@ -0,0 +1,14 @@
+touch inventory playbook.yml
+
+mkdir roles
+cd roles
+
+mkdir webserver
+cd webserver
+
+mkdir tasks handlers defaults files
+
+touch tasks/main.yml
+touch handlers/main.yml
+touch defaults/main.yml
+touch files/index.html
